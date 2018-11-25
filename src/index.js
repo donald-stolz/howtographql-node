00ddbed8a92ac6e58a5b7ac8d107b355e0cc98a1
Donald Stolz <donaldtstolz@gmail.com>
2018-11-25 15:40:01 -0800

    Added authentication; Finished section 6

--- src/index.js
+++ src/index.js
@@ -1,43 +1,13 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { Prisma } = require('prisma-binding');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const AuthPayload = require('./resolvers/AuthPayload');
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: () => {
-            return context.db.query.links({}, info);
-        },
-    },
-    Mutation: {
-        // 2
-        post: (root, args, context, info) => {
-            return context.db.mutation.createLink(
-                {
-                    data: {
-                        url: args.url,
-                        description: args.description,
-                    },
-                },
-                info
-            );
-        },
-        // updateLink: (root, args) => {
-        //     let index = links.findIndex(link => link.id == args.id);
-        //     let temp = links[index];
-        //     if (temp) {
-        //         temp.description = args.description
-        //             ? args.description
-        //             : temp.description;
-        //         temp.url = args.url ? args.url : temp.url;
-        //         links[index] = temp;
-        //     }
-        //     return temp;
-        // },
-        // deleteLink: (root, args) => {
-        //     let index = links.findIndex(link => link === args.id);
-        //     links.splice(index, 1);
-        // },
-    },
+    Query,
+    Mutation,
+    AuthPayload,
 };
 
 const server = new GraphQLServer({
