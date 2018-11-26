557fb4bfb3ff0d639c83de91dea2afe25b791998
Donald Stolz <donaldtstolz@gmail.com>
2018-11-25 17:32:27 -0800

    Added subscriptions

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const { Prisma } = require('prisma-binding');
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const AuthPayload = require('./resolvers/AuthPayload');
+const Subscription = require('./resolvers/Subscription');
 
 const resolvers = {
     Query,
     Mutation,
     AuthPayload,
+    Subscription,
 };
 
 const server = new GraphQLServer({
