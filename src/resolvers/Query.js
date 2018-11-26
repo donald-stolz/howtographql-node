41855ca95e763d3be829ca2c5e6e4be90114d205
Donald Stolz <donaldtstolz@gmail.com>
2018-11-25 17:42:24 -0800

    Update schema & query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,13 @@
 function feed(parent, args, context, info) {
-    return context.db.query.links({}, info);
+    const where = args.filter
+        ? {
+              OR: [
+                  { url_contains: args.filter },
+                  { description_contains: args.filter },
+              ],
+          }
+        : {};
+    return context.db.query.links({ where }, info);
 }
 
 module.exports = {
