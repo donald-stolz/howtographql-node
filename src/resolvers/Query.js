ebb9c6b12773bb33a68a09608c6b07ac69831c83
Donald Stolz <donaldtstolz@gmail.com>
2018-11-26 09:23:33 -0800

    Added pagination & sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,4 @@
-function feed(parent, args, context, info) {
+async function feed(parent, args, context, info) {
     const where = args.filter
         ? {
               OR: [
@@ -7,7 +7,28 @@ function feed(parent, args, context, info) {
               ],
           }
         : {};
-    return context.db.query.links({ where }, info);
+
+    const queriedLinkes = await context.db.query.links(
+        { where, skip: args.skip, first: args.first, orderBy: args.orderBy },
+        `{ id }`
+    );
+
+    const countSelectionSet = `
+    {
+      aggregate {
+        count
+      }
+    }
+  `;
+    const linksConnection = await context.db.query.linksConnection(
+        {},
+        countSelectionSet
+    );
+
+    return {
+        count: linksConnection.aggregate.count,
+        linkIds: queriedLinkes.map(link => link.id),
+    };
 }
 
 module.exports = {
